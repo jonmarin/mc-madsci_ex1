e0c3fb8ea5abb64c479d44285062ff47aeb47b53
jonmarin <jonsonmarin@gmail.com>
2020-02-29 21:53:29 -0500

    Added a custom.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+input.onButtonPressed(Button.A, function () {
+    music.playTone(440, music.beat(BeatFraction.Whole))
+})
+input.onGesture(Gesture.Shake, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.B, function () {
+    music.playTone(392, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playTone(349, music.beat(BeatFraction.Whole))
+})
